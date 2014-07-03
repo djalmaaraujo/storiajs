235fe9a7fecf57872d73e70550577ef0c3c13f25
djalmaaraujo <djalma.araujo@gmail.com>
2014-07-03 15:58:37 -0300

    refactoring tests

--- test/api_test.js
+++ test/api_test.js
@@ -5,62 +5,58 @@ var defaults = {
   bar: "baz"
 };
 
-describe("Api", function(){
-  describe("#constructor", function () {
-    it("expect to have a list of default options", function () {
-      var Storia = new StoriaAPI();
-      expect(Storia.defaults).to.deep.equal(defaults);
+describe("Api", function() {
+  var m = {};
+
+  beforeEach(function () {
+    m.Storia = new StoriaAPI();
+  });
+
+  describe("#constructor", function() {
+    it("expect to have a list of default options", function() {
+      expect(m.Storia.defaults).to.deep.equal(defaults);
     });
   });
 
-  describe("#setup", function(){
-    it("expect to have the setup method", function(){
-      var Storia = new StoriaAPI();
-      expect(typeof Storia.setup).to.be.eq("function");
+  describe("#setup", function() {
+    it("expect to have the setup method", function() {
+      expect(typeof m.Storia.setup).to.be.eq("function");
     });
 
-    it("expect to pass options and overwrite the default options", function () {
-      var Storia = new StoriaAPI();
-      Storia.setup({bar: "gud"});
+    it("expect to pass options and overwrite the default options", function() {
+      m.Storia.setup({bar: "gud"});
 
-      expect(Storia.defaults).to.deep.equal({bar: "gud"});
+      expect(m.Storia.defaults).to.deep.equal({bar: "gud"});
     });
 
-    it("expect that addHandlerTo returns the instance of Storia library", function () {
-      var Storia = new StoriaAPI();
-
-      expect(Storia.setup({bar: "gud"})).to.be.an.instanceof(StoriaAPI);
+    it("expect that addHandlerTo returns the instance of Storia library", function() {
+      expect(m.Storia.setup({bar: "gud"})).to.be.an.instanceof(StoriaAPI);
     });
   });
 
-  describe("#addHandlerTo", function(){
-    it("expect to have the addHandlerTo method", function () {
-      var Storia = new StoriaAPI();
-      expect(typeof Storia.addHandlerTo).to.be.eq("function");
+  describe("#addHandlerTo", function() {
+    it("expect to have the addHandlerTo method", function() {
+      expect(typeof m.Storia.addHandlerTo).to.be.eq("function");
     });
 
-    it("expect to have and array of handlers", function () {
-      var Storia = new StoriaAPI();
-      expect(Storia.handlers).to.be.an.instanceof(Array);
+    it("expect to have and array of handlers", function() {
+      expect(m.Storia.handlers).to.be.an.instanceof(Array);
     });
 
-    it("expect to add an item to the list of handlers", function () {
-      var Storia = new StoriaAPI();
-      Storia.addHandlerTo(".my-element");
+    it("expect to add an item to the list of handlers", function() {
+      m.Storia.addHandlerTo(".my-element");
 
-      expect(Storia.handlers).to.deep.equal([".my-element"]);
+      expect(m.Storia.handlers).to.deep.equal([".my-element"]);
     });
 
-    it("expect to dont add if handler parameter is empty", function () {
-      var Storia = new StoriaAPI();
-      Storia.addHandlerTo();
+    it("expect to dont add if handler parameter is empty", function() {
+      m.Storia.addHandlerTo();
 
-      expect(Storia.handlers).to.deep.equal([]);
+      expect(m.Storia.handlers).to.deep.equal([]);
     });
 
-    it("expect that addHandlerTo returns the instance of Storia library", function () {
-      var Storia = new StoriaAPI();
-      expect(Storia.addHandlerTo()).to.be.an.instanceof(StoriaAPI);
+    it("expect that addHandlerTo returns the instance of Storia library", function() {
+      expect(m.Storia.addHandlerTo()).to.be.an.instanceof(StoriaAPI);
     });
   });
 });
